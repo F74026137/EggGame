b7cad9f57df2f46dd4c03219e049f3c61da6d297
F74026137 <ann831210@gmail.com>
2016-05-21 15:06:25 +0800

    0521

---
+++ src/project/Game.java
@@ -0,0 +1,12 @@
+package project;
+
+public interface Game {
+	
+	public void run();
+	public void close();
+	public String result();
+	public boolean isRunning();
+	boolean running=false;
+	
+
+}
---
+++ src/project/Ox.java
@@ -0,0 +1,24 @@
+package project;
+
+public class Ox {
+	boolean running;
+	public void run(){
+		running=true;
+		
+		
+		
+	}
+	public void close(){
+		
+	}
+	public boolean isRunning(){
+		return running;
+	}
+	
+	public String result(){
+		String res="";
+		
+		return res;
+	}
+
+}
